d886380703f54d84823cdea84cc43db064a2a0ff
dogatech <dogatech@dogatech.home>
2023-08-28 23:02:07 -0700

    get ratings showing with icons

--- src/components/SongListItem.tsx
+++ src/components/SongListItem.tsx
@@ -1,17 +1,27 @@
-import type { Component } from 'solid-js';
+import { For, type Component, Index, Show } from 'solid-js';
 import { Song } from './SongList';
-import styles from './SongListItem.module.css';
+import { ImStarEmpty, ImStarFull } from 'solid-icons/im';
 
 const SongListItem: Component<{song: Song}> = (props) => {
   return (
     <tr>
-      <td class="flex flex-row">
+      <td class="flex flex-row justify-between">
         <span>
           <span>{props.song.artist}</span>
           <span> - </span>
           <span><b>{props.song.title}</b></span>
         </span>
-        <span class="">{props.song.rating}</span>
+        <span class="flex flex-row text-primary items-center">
+          <For each={[...Array(5).keys()]}>
+            {
+              (i) => {
+                return <Show when={i < (props.song.rating || 0)} fallback={<ImStarEmpty children />}>
+                        <ImStarFull class="fill-secondary"/>
+                      </Show>
+              }
+            }
+          </For>
+        </span>
       </td>
     </tr>
   );
