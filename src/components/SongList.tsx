b993902dd16820305d7b421bddf1b9bb79468f95
dogatech <dogatech@dogatech.home>
2023-11-22 01:08:21 -0600

    add dev mode

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,4 +1,4 @@
-import { type Component, createEffect, Index } from 'solid-js';
+import { type Component, createEffect, Index, DEV } from 'solid-js';
 import SongListItem from './SongListItem';
 import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import { db } from '../App';
@@ -11,9 +11,11 @@ import Song, { songConverter } from '../dataclasses/Song';
 const SongList: Component = () => {
   const {songs, setSongs} = SongsConsumer();
   createEffect(async () => {
+    console.log("is dev: ", DEV);
+    let max = !DEV ? 20 : 3;
     const q = !!searchQuery()
-        ? query(collection(db, 'songs').withConverter(songConverter), where(searchField(), '>=', searchQuery()), where(searchField(), '<=', searchQuery()+'\uf8ff'), limit(6))
-        : query(collection(db, 'songs').withConverter(songConverter), limit(6));
+        ? query(collection(db, 'songs').withConverter(songConverter), where(searchField(), '>=', searchQuery()), where(searchField(), '<=', searchQuery()+'\uf8ff'), limit(max))
+        : query(collection(db, 'songs').withConverter(songConverter), limit(max));
     const snapshot = await getDocs(q);
     let songList: Song[] = []
     snapshot.forEach((doc) => {
