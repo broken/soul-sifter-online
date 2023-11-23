e941195826965aa89cec19e5de6c504c3dba68cf
dogatech <dogatech@dogatech.home>
2023-11-22 22:39:22 -0600

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,68 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
+import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa'
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  base: '/',
+  includeAssets: ['favicon.svg', 'icon_*.png'],
+  manifest: {
+    name: "Soul Sifter Online",
+    short_name: "SSO",
+    theme_color: '#ffffff',
+    start_url: "https://soul-sifter.web.app/",
+    display: "standalone",
+    icons: [
+      {
+        src: 'assets/icon_192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/assets/icon_512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/icon_512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    screenshots: [
+      {
+        src: '/assets/screenshot_narrow.png',
+        type: 'image/png',
+        sizes: '596x814',
+        form_factor: 'narrow',
+      },
+      {
+        src: '/assets/screenshot_wide.png',
+        type: 'image/png',
+        sizes: '947x512',
+        form_factor: 'wide',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
     solidPlugin(),
+    VitePWA(pwaOptions),
   ],
   server: {
     port: 3000,
