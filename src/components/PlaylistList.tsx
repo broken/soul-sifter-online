bff8bf53f69ff439c3ae901a6e374f8957dfdbbc
dogatech <dogatech@dogatech.home>
2024-02-03 00:22:06 -0800

    add swipe to open youtube

--- src/components/PlaylistList.tsx
+++ src/components/PlaylistList.tsx
@@ -2,6 +2,7 @@ import { type Component, createEffect, Index, createSignal, DEV, Show } from 'so
 import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from '../App';
 import Playlist, { playlistConverter } from '../dataclasses/Playlist';
+import styles from './PlaylistList.module.css';
 
 
 const PlaylistList: Component = () => {
@@ -13,6 +14,23 @@ const PlaylistList: Component = () => {
     }
     const appLink = `https://music.youtube.com/playlist?list=${playlistId}`;
     window.open(appLink);
+  }
+  const handleSwipe = (playlistId: string | undefined, event: TouchEvent) => {
+    console.log('swiped');
+    console.log(event);
+    // define the minimum distance to trigger the action
+    const minDistance = 80;
+    const target = event.target as Element;
+    const container = target.parentElement;
+    if (container == null) return;
+    // get the distance the user swiped
+    const swipeDistance = container.scrollLeft;
+    console.log(`swiped distance of ${swipeDistance} = ${container.scrollLeft} - ${target.clientWidth}`);
+    if (swipeDistance > minDistance) {
+      openPlaylist(playlistId);
+    } else {
+      console.log(`did not swipe ${minDistance}px`);
+    }
   }
   createEffect(async () => {
     const q = query(collection(db, 'playlists').withConverter(playlistConverter));
@@ -27,7 +45,6 @@ const PlaylistList: Component = () => {
     setPlaylists(playlistList);
     if (!!DEV) console.log(playlistList);
   });
-
   return (
     <div class="overflow-x-hidden overflow-y-scroll w-screen" style="height: calc(100vh - 128px);">
       <table class="table">
@@ -35,22 +52,29 @@ const PlaylistList: Component = () => {
           <Index each={playlists()}>
             {playlist => (
               <tr onclick={() => openPlaylist(playlist().youtubeId)}>
-                <td class="flex flex-row justify-between px-7 py-4">
-                  <span class="flex flex-row">
-                    <span><b>{playlist().name}</b></span>
-                    <Show when={!!playlist().query}>
-                      <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
-                      </svg>
-                    </Show>
-                  </span>
-                  <span class="flex flex-row">
-                    <Show when={!!playlist().youtubeId}>
-                      <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd"/>
-                      </svg>
-                    </Show>
-                  </span>
+                <td class={`px-0 py-0 ${styles["swipe-container"]}`} ontouchend={[handleSwipe, playlist().youtubeId]}>
+                  <div class={`flex flex-row justify-between px-7 py-4 ${styles["swipe-element"]}`}>
+                    <span class="flex flex-row">
+                      <span><b>{playlist().name}</b></span>
+                      <Show when={!!playlist().query}>
+                        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                        </svg>
+                      </Show>
+                    </span>
+                    <span class="flex flex-row">
+                      <Show when={!!playlist().youtubeId}>
+                        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                          <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd"/>
+                        </svg>
+                      </Show>
+                    </span>
+                  </div>
+                  <div class={`px-1 py-4 ${styles.action} ${styles.right}`}>
+                    <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                      <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd"/>
+                    </svg>
+                  </div>
                 </td>
               </tr>
             )}
