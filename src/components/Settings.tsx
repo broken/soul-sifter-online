379dd523dd7deffaba8885d9d1162f5559a66e3a
dogatech <dogatech@dogatech.home>
2024-02-04 15:14:35 -0800

    fix settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,207 +1,32 @@
-import { Show, type Component, createSignal, createEffect } from "solid-js";
+import { Show, type Component, createSignal, createEffect, Index } from "solid-js";
 
 const Settings: Component = () => {
   const [theme, setTheme] = createSignal('soul');
   createEffect(() => {
     document.querySelector('html')!.setAttribute('data-theme', theme());
   });
+  const themes: string[] = ['default', 'light', 'dark', 'soul', 'sifter', 'acid', 'aqua', 'autumn', 'black', 'bumblebee', 'business',
+                            'cmyk', 'coffee', 'corporate', 'cupcake', 'cyberpunk', 'dracula', 'emerald', 'fantasy', 'forest', 'garden',
+                            'halloween', 'lemonade', 'lofi', 'luxury', 'night', 'pastel', 'retro', 'synthwave', 'winter', 'wireframe'];
+
   return (
-    <Show when={true}>
-      <div class="absolute h-screen w-screen bg-base-100 flex flex-col justify-center items-center">
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">Default</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onClick={() => setTheme('default')} value="default"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">light</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('light')} value="light"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">dark</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('dark')} value="dark"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">soul</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('soul')} value="soul"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">sifter</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('sifter')} value="sifter"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">acid</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('acid')} value="acid"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">aqua</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('aqua')} value="aqua"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">autumn</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('autumn')} value="autumn"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">black</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('black')} value="black"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">bumblebee</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('bumblebee')} value="bumblebee"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">business</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('business')} value="business"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">cmyk</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('cmyk')} value="cmyk"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">coffee</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('coffee')} value="coffee"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">corporate</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('corporate')} value="corporate"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">cupcake</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('cupcake')} value="cupcake"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">cyberpunk</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('cyberpunk')} value="cyberpunk"/>
-          </label>
-        </div>
-        {/* <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">dracula</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('dracula')} value="dracula"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">emerald</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('emerald')} value="emerald"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">fantasy</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('fantasy')} value="fantasy"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">forest</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('forest')} value="forest"/>
-          </label>
-        </div> */}
-        {/* <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">garden</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('garden')} value="garden"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">halloween</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('halloween')} value="halloween"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">lemonade</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('lemonade')} value="lemonade"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">lofi</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('lofi')} value="lofi"/>
-          </label>
-        </div> */}
-        {/* <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">luxury</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('luxury')} value="luxury"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">night</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('night')} value="night"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">pastel</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('pastel')} value="pastel"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">retro</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('retro')} value="retro"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">synthwave</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('synthwave')} value="synthwave"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">valentine</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('valentine')} value="valentine"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">winter</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('winter')} value="winter"/>
-          </label>
-        </div>
-        <div class="form-control">
-          <label class="label cursor-pointer gap-4">
-            <span class="label-text">wireframe</span>
-            <input type="radio" name="theme-radios" class="radio theme-controller" onclick={() => setTheme('wireframe')} value="wireframe"/>
-          </label>
-        </div> */}
-      </div>
-    </Show>
+    <div class="overflow-x-hidden overflow-y-scroll w-screen" style="height: calc(100vh - 128px);">
+      <table class="table">
+        <tbody>
+          <Index each={themes}>
+            {t => (
+              <tr onclick={() => setTheme(t())}>
+                <td class="px-0 py-0">
+                  <div class="flex flex-row justify-between px-7 py-4">
+                    <span class="label-text">{t()}</span>
+                  </div>
+                </td>
+              </tr>
+            )}
+          </Index>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
