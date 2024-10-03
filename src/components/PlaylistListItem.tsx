a69f2c978ebccc95749a98342850e45e9aaf557a
dogatech <dogatech@dogatech.home>
2024-10-02 23:36:45 -0700

    only allow swipes on playlists with youtubeid

--- src/components/PlaylistListItem.tsx
+++ src/components/PlaylistListItem.tsx
@@ -21,8 +21,6 @@ const PlaylistListItem: Component<{playlist: Playlist}> = (props) => {
   }
 
   const handleSwipe = (playlistId: string | undefined, event: TouchEvent) => {
-    console.log('swiped')
-    console.log(event)
     // define the minimum distance to trigger the action
     const minDistance = 80
     const target = event.target as Element
@@ -40,7 +38,7 @@ const PlaylistListItem: Component<{playlist: Playlist}> = (props) => {
 
   return (
     <tr onclick={() => setActivePlaylist(props.playlist)}  classList={{ [styles.active]: isActive() }}>
-      <td class={`px-0 py-0 ${styles["swipe-container"]}`} ontouchend={[handleSwipe, props.playlist.youtubeid]}>
+      <td class={`px-0 py-0`} classList={{ [styles.swipe_container]: !!props.playlist.youtubeid }} ontouchend={[handleSwipe, props.playlist.youtubeid]}>
         <div class={`flex flex-row justify-between px-7 py-4 ${styles["swipe-element"]}`}>
           <span class="flex flex-row">
             <span>{props.playlist.name}</span>
@@ -58,11 +56,13 @@ const PlaylistListItem: Component<{playlist: Playlist}> = (props) => {
             </Show>
           </span>
         </div>
-        <div class={`px-1 py-4 ${styles.action} ${styles.right}`}>
-          <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd"/>
-          </svg>
-        </div>
+        <Show when={props.playlist.youtubeid}>
+          <div class={`px-1 py-4 ${styles.action} ${styles.right}`}>
+            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M21.7 8c0-.7-.4-1.3-.8-2-.5-.5-1.2-.8-2-.8C16.2 5 12 5 12 5s-4.2 0-7 .2c-.7 0-1.4.3-2 .9-.3.6-.6 1.2-.7 2l-.2 3.1v1.5c0 1.1 0 2.2.2 3.3 0 .7.4 1.3.8 2 .6.5 1.4.8 2.2.8l6.7.2s4.2 0 7-.2c.7 0 1.4-.3 2-.9.3-.5.6-1.2.7-2l.2-3.1v-1.6c0-1 0-2.1-.2-3.2ZM10 14.6V9l5.4 2.8-5.4 2.8Z" clip-rule="evenodd"/>
+            </svg>
+          </div>
+        </Show>
       </td>
     </tr>
   )
