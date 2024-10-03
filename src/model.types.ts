06d66d56d287e485217052f304c1bf81e24ced17
dogatech <dogatech@dogatech.home>
2024-10-02 17:21:03 -0700

    create custom model types

---
+++ src/model.types.ts
@@ -0,0 +1,31 @@
+import { Tables } from './database.types';
+
+type Album = Tables<'albums'>;
+type AlbumPart = Tables<'albumparts'>;
+type BasicGenre = Tables<'basicgenres'>;
+type Change = Tables<'changes'>;
+type Mix = Tables<'mixes'>;
+type MusicVideo = Tables<'musicvideos'>;
+type Playlist = Tables<'playlists'>;
+type PlaylistEntry = Tables<'playlistentries'>;
+type PlaylistStyle = Tables<'playliststyles'>;
+type Song = Tables<'songs'>;
+type SongStyle = Tables<'songstyles'>;
+type Style = Tables<'styles'>;
+type StyleChildren = Tables<'stylechildren'>;
+
+export type {
+  Album,
+  AlbumPart,
+  BasicGenre,
+  Change,
+  Mix,
+  MusicVideo,
+  Playlist,
+  PlaylistEntry,
+  PlaylistStyle,
+  Song,
+  SongStyle,
+  Style,
+  StyleChildren
+};
