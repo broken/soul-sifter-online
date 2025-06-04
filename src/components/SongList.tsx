7058c564a430f4f1103ab608d6242d77adda0c2c
dogatech <dogatech@dogatech.home>
2025-06-04 03:02:16 -0700

    fix multiple genres showing in filter list

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -222,7 +222,12 @@ const SongList: Component = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
-            <span>Genre {activeGenres()[0].name} ({activeGenres()[0].id}).</span>
+            <span style="display: inline-block; max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+              {activeGenres().length === 1
+                ? `Genre ${activeGenres()[0].name} (${activeGenres()[0].id}).`
+                : activeGenres().map(g => g.name).join(', ') + '.'
+              }
+            </span>
             <button class="btn btn-sm bg-info text-info-content" onclick={() => setActiveGenres([])}>Clear</button>
           </div>
         </div>
