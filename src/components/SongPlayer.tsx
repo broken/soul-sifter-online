ed9f5ca9bacf957dc645ba88dd7aae59cc96a3b0
dogatech <dogatech@dogatech.home>
2026-08-19 23:03:27 -0700

    center the play button in the SongPlayer

--- src/components/SongPlayer.tsx
+++ src/components/SongPlayer.tsx
@@ -358,7 +358,7 @@ const SongPlayer: Component<SongPlayerProps> = (props) => {
               <Show
                 when={isPlaying()}
                 fallback={
-                  <svg class="w-4 h-4 translate-x-0.5" fill="currentColor" viewBox="0 0 24 24">
+                  <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M8 5v14l11-7z" />
                   </svg>
                 }
