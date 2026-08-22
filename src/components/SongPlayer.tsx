573c671b7e5dd41ca47c4d83248f368b195d4f31
dogatech <dogatech@dogatech.home>
2026-08-21 17:31:04 -0700

    fix play/pause button css

--- src/components/SongPlayer.tsx
+++ src/components/SongPlayer.tsx
@@ -735,12 +735,12 @@ const SongPlayer: Component<SongPlayerProps> = (props) => {
               <Show
                 when={isPlaying()}
                 fallback={
-                  <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+                  <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M8 5v14l11-7z" />
                   </svg>
                 }
               >
-                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
                 </svg>
               </Show>
